7647aebf4c21c894e53655ed8a8da04589ccb5ce
Piotr Kasperek <kasperek@Piotrs-MacBook-Pro.local>
2022-01-23 00:46:38 +0100

    adding first components (userslist, userslistitem)

---
+++ src/data/users.js
@@ -0,0 +1,31 @@
+export const users = [
+  {
+    name: 'Piotr Kasperek',
+    attendance: '45%',
+    average: '3.4',
+  },
+  {
+    name: 'Adam Romański',
+    attendance: '34%',
+    average: '2.5',
+  },
+  {
+    name: 'Maja Włodarkiewicz',
+    attendance: '67%',
+    average: '4.2',
+  },
+  {
+    name: 'Roman Wałczykiewicz',
+    attendance: '56%',
+    average: '3.3',
+  },
+  {
+    name: 'Harnold Harnasiewicz',
+    attendance: '26%',
+    average: '2.1',
+  },
+  {
+    name: 'Maciek Durszewicz',
+    average: '1.5',
+  },
+];
